f9946b9a49c583243f0504c6ebc6ff0cdf5804a2
laborifermc <laborifermc@gmail.com>
2025-04-15 16:02:38 +0200

    test

--- myappvictor/myappvictor-ebpf/src/main.rs
+++ myappvictor/myappvictor-ebpf/src/main.rs
@@ -7,43 +7,47 @@ use aya_ebpf::{
     programs::XdpContext,
 };
 use aya_log_ebpf::info;
+use core::mem;
 
 #[xdp]
 pub fn xdp_hello(ctx: XdpContext) -> u32 {
-    match unsafe { try_xdp_hello(ctx) } {
+    match unsafe { try_xdp_hello(&ctx) } {
         Ok(ret) => ret,
         Err(_) => xdp_action::XDP_ABORTED,
     }
 }
 
-unsafe fn try_xdp_hello(ctx: XdpContext) -> Result<u32, u32> {
-    // On récupère les données du paquet
-    let data = ctx.data();
-    let data_end = ctx.data_end();
-    let len = data_end.offset_from(data);
+unsafe fn try_xdp_hello(ctx: &XdpContext) -> Result<u32, u32> {
+    let data = ctx.data() as *const u8;
+    let data_end = ctx.data_end() as *const u8;
 
-    // Vérifie qu'on a assez de données pour un header Ethernet + IP (approximatif)
-    if len < 34 {
+    // Check bounds for Ethernet header (14 bytes)
+    if data.add(14) > data_end {
         return Err(1);
     }
 
-    // Interprète la mémoire comme un header Ethernet (14 octets)
+    // Get ethertype (bytes 12-13)
     let eth_proto_ptr = data.add(12) as *const u16;
+    if eth_proto_ptr.add(1) as *const u8 > data_end {
+        return Err(1);
+    }
     let eth_proto = u16::from_be(*eth_proto_ptr);
 
-    // Si c'est un paquet IPv4 (0x0800), lis le protocole IP (byte 23 du header IP)
     if eth_proto == 0x0800 {
-        let ip_proto_ptr = data.add(23) as *const u8;
-        let ip_proto = *ip_proto_ptr;
+        // IPv4: check bounds for IP header protocol (byte 23)
+        if data.add(23 + 1) > data_end {
+            return Err(1);
+        }
+        let ip_proto = *data.add(23);
 
         match ip_proto {
-            1 => info!(&ctx, "IPv4: ICMP packet"),
-            6 => info!(&ctx, "IPv4: TCP packet"),
-            17 => info!(&ctx, "IPv4: UDP packet"),
-            _ => info!(&ctx, "IPv4: other protocol {}", ip_proto),
+            1 => info!(ctx, "IPv4: ICMP packet"),
+            6 => info!(ctx, "IPv4: TCP packet"),
+            17 => info!(ctx, "IPv4: UDP packet"),
+            _ => info!(ctx, "IPv4: other protocol {}", ip_proto),
         }
     } else {
-        info!(&ctx, "Non-IPv4 packet, ethertype: {:#x}", eth_proto);
+        info!(ctx, "Non-IPv4 packet, ethertype: {}", eth_proto);
     }
 
     Ok(xdp_action::XDP_PASS)

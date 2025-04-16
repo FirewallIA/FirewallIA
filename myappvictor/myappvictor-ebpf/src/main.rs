bf9bced80063ecf07a52f89c76c295fb8035cfb0
laborifermc <laborifermc@gmail.com>
2025-04-16 10:00:32 +0200

    info when ping

--- myappvictor/myappvictor-ebpf/src/main.rs
+++ myappvictor/myappvictor-ebpf/src/main.rs
@@ -22,11 +22,11 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 pub fn xdp_firewall(ctx: XdpContext) -> u32 {
     match try_xdp_firewall(ctx) {
         Ok(ret) => ret,
-        Err(_) => xdp_action::XDP_PASS, // XDP_ABORTED
+        Err(_) => xdp_action::XDP_PASS,
     }
 }
 
-#[inline(always)] // 
+#[inline(always)]
 fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
     let start = ctx.data();
     let end = ctx.data_end();
@@ -40,7 +40,7 @@ fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
 }
 
 fn try_xdp_firewall(ctx: XdpContext) -> Result<u32, ()> {
-    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?; // 
+    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?;
     match unsafe { (*ethhdr).ether_type } {
         EtherType::Ipv4 => {}
         _ => return Ok(xdp_action::XDP_PASS),
@@ -49,22 +49,22 @@ fn try_xdp_firewall(ctx: XdpContext) -> Result<u32, ()> {
     let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
     let source_addr = u32::from_be(unsafe { (*ipv4hdr).src_addr });
 
-    let source_port = match unsafe { (*ipv4hdr).proto } {
+    match unsafe { (*ipv4hdr).proto } {
         IpProto::Tcp => {
-            let tcphdr: *const TcpHdr =
-                ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
-            u16::from_be(unsafe { (*tcphdr).source })
+            let tcphdr: *const TcpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            let source_port = u16::from_be(unsafe { (*tcphdr).source });
+            info!(&ctx, "TCP SRC IP: {:i}, SRC PORT: {}", source_addr, source_port);
         }
         IpProto::Udp => {
-            let udphdr: *const UdpHdr =
-                ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
-            u16::from_be(unsafe { (*udphdr).source })
+            let udphdr: *const UdpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            let source_port = u16::from_be(unsafe { (*udphdr).source });
+            info!(&ctx, "UDP SRC IP: {:i}, SRC PORT: {}", source_addr, source_port);
         }
-        _ => return Err(()),
-    };
-
-    // 
-    info!(&ctx, "SRC IP: {:i}, SRC PORT: {}", source_addr, source_port);
+        IpProto::Icmp => {
+            info!(&ctx, "ICMP SRC IP: {:i}", source_addr);
+        }
+        _ => return Ok(xdp_action::XDP_PASS),
+    }
 
     Ok(xdp_action::XDP_PASS)
 }

4612945ed69b40a0afe8e4bf11545e7186a7d0c9
laborifermc <laborifermc@gmail.com>
2025-04-16 13:34:36 +0200

    Droping package

--- myappvictor/myappvictor/build.rs
+++ myappvictor/myappvictor/build.rs
@@ -8,9 +8,18 @@ fn main() -> anyhow::Result<()> {
             .no_deps()
             .exec()
             .context("MetadataCommand::exec")?;
+
     let ebpf_package = packages
         .into_iter()
         .find(|cargo_metadata::Package { name, .. }| name == "myappvictor-ebpf")
         .ok_or_else(|| anyhow!("myappvictor-ebpf package not found"))?;
-    aya_build::build_ebpf([ebpf_package], Toolchain::default())
+
+    println!("Building eBPF package: {:?}", ebpf_package.name);
+    let result = aya_build::build_ebpf([ebpf_package], Toolchain::default());
+
+    if let Err(ref e) = result {
+        println!("eBPF build failed: {:?}", e);
+    }
+
+    result
 }

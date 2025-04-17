8f2b099e58cd213b041be0b0685acc8358704896
laborifermc <laborifermc@gmail.com>
2025-04-17 10:21:59 +0200

    Port and ip block try

--- xdp-drop/xdp-drop-ebpf/src/main.rs
+++ xdp-drop/xdp-drop-ebpf/src/main.rs
@@ -64,9 +64,9 @@ unsafe fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
 }
 
 // (2)
-fn block_ip_port(ip: u32, port: u16) -> bool {
+fn block_ip_port(addr: u32, port: u16) -> bool {
     let key = IpPort {
-        ip,
+        addr,
         port,
     };
     unsafe { BLOCKLIST.get(&key).is_some() }

54b4168d08074a8fe079062d2670eed2d06261af
laborifermc <laborifermc@gmail.com>
2025-04-17 10:48:46 +0200

    Port and ip block try

--- xdp-drop/xdp-drop-ebpf/src/main.rs
+++ xdp-drop/xdp-drop-ebpf/src/main.rs
@@ -30,7 +30,7 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 
 // Hashmap pour ip et port 
 #[repr(C)]
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+#[derive(Debug, Clone, Copy)]
 pub struct IpPort {
     pub addr: u32,
     pub port: u16,

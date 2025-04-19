b7dcfca80a5c85170e4bf3904795de338a4de821
laborifermc <laborifermc@gmail.com>
2025-04-19 22:12:49 +0200

    Mise en place de gRPC

--- xdp-drop/xdp-drop/build.rs
+++ xdp-drop/xdp-drop/build.rs
@@ -12,4 +12,14 @@ fn main() -> anyhow::Result<()> {
         .find(|cargo_metadata::Package { name, .. }| name == "xdp-drop-ebpf")
         .ok_or_else(|| anyhow!("xdp-drop-ebpf package not found"))?;
     aya_build::build_ebpf([ebpf_package], Toolchain::default())
+    
+    tonic_build::configure()
+    .build_server(true)  // Générer le serveur gRPC
+    .build_client(true)  // Générer le client gRPC
+    .out_dir("src")      // Dossier où les fichiers générés seront placés
+    .compile(
+        &["proto/firewall.proto"],  // Chemin vers ton fichier .proto
+        &["proto"],                  // Dossier contenant les fichiers .proto
+    )
+    .expect("Échec de la compilation du fichier .proto");
 }

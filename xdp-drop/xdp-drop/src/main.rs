3fc55627019357f402b92ff9b2621e624b5effd4
laborifermc <laborifermc@gmail.com>
2025-05-19 09:18:55 +0200

    comment import 'Empty'

--- xdp-drop/xdp-drop/src/main.rs
+++ xdp-drop/xdp-drop/src/main.rs
@@ -14,7 +14,7 @@ use tonic::{transport::Server, Request, Response, Status};
 use xdp_drop_common::IpPort;
 use crate::firewall::firewall_service_server::FirewallService;
 use crate::firewall::firewall_service_server::FirewallServiceServer;
-use crate::firewall::Empty;
+//use crate::firewall::Empty;
 
 // Import du proto compilé gRPC
 tonic::include_proto!("firewall");

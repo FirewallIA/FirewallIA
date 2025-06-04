78eae1dde4d24ca3918e331faf2b96fbfedb887a
laborifermc <laborifermc@gmail.com>
2025-06-04 15:57:25 +0200

    fix statefull error

--- xdp-drop/xdp-drop-common/src/lib.rs
+++ xdp-drop/xdp-drop-common/src/lib.rs
@@ -1,48 +1,44 @@
+// Dans xdp-drop-common/src/lib.rs
+
 #![no_std]
 
-// L'importation de `aya::Pod` est pour le *trait*.
-// La *macro* `derive(Pod)` est généralement rendue disponible via la feature "macros" de la crate `aya`.
 #[cfg(feature = "user")]
-use aya::Pod; // C'est pour le TRAIT Pod.
+use aya::Pod as AyaPodTrait; // Importez le TRAIT Pod, vous pouvez le renommer si nécessaire pour éviter la confusion
 
-// Si vous avez besoin d'importer explicitement la macro pour `derive`
-// (normalement ce n'est pas nécessaire si les features sont bien configurées
-// dans Cargo.toml pour la crate `aya` elle-même)
-// #[cfg(feature = "user")]
-// use aya::macros::Pod; // Cela importerait la MACRO Pod. Essayons sans d'abord.
-
-
-// --- Structure PacketLog (EXISTANTE) ---
+// --- Structure PacketLog ---
 #[repr(C)]
 #[derive(Clone, Copy)]
+// Si vous voulez utiliser derive pour PacketLog (et que vous avez supprimé l'impl manuelle)
+#[cfg_attr(feature = "user", derive(aya::Pod))] // Modifié ici
 pub struct PacketLog {
     pub ipv4_address: u32,
     pub action: u32,
 }
+// Si vous utilisez derive, supprimez :
+// #[cfg(feature = "user")]
+// unsafe impl AyaPodTrait for PacketLog {}
 
-#[cfg(feature = "user")]
-unsafe impl Pod for PacketLog {} // Implémentation manuelle du TRAIT
 
-
-// --- Structure IpPort (EXISTANTE, avec potentiels ajouts de traits) ---
+// --- Structure IpPort ---
 #[repr(C)]
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+#[cfg_attr(feature = "user", derive(aya::Pod))] // Modifié ici
 pub struct IpPort {
     pub addr: u32,
     pub addr_dest: u32,
     pub port: u16,
     pub _pad: u16,
 }
-
-#[cfg(feature = "user")]
-unsafe impl Pod for IpPort {} // Implémentation manuelle du TRAIT
+// Si vous utilisez derive, supprimez :
+// #[cfg(feature = "user")]
+// unsafe impl AyaPodTrait for IpPort {}
 
 
 // --- NOUVELLES STRUCTURES POUR LE SUIVI DE CONNEXION (STATEFUL) ---
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "user", derive(aya::macros::Pod))] // UTILISEZ LE CHEMIN COMPLET VERS LA MACRO
+#[cfg_attr(feature = "user", derive(aya::Pod))] // Modifié ici
 pub struct ConnectionKey {
     pub src_ip: u32,
     pub src_port: u16,
@@ -55,7 +51,7 @@ pub struct ConnectionKey {
 
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "user", derive(aya::macros::Pod))] // UTILISEZ LE CHEMIN COMPLET VERS LA MACRO
+#[cfg_attr(feature = "user", derive(aya::Pod))] // Modifié ici
 pub enum TcpState {
     SynSent = 1,
     SynReceived = 2,
@@ -65,7 +61,7 @@ pub enum TcpState {
 
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "user", derive(aya::macros::Pod))] // UTILISEZ LE CHEMIN COMPLET VERS LA MACRO
+#[cfg_attr(feature = "user", derive(aya::Pod))] // Modifié ici
 pub enum UdpState {
     New = 1,
     Established = 2,
@@ -73,7 +69,7 @@ pub enum UdpState {
 
 #[repr(C)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "user", derive(aya::macros::Pod))] // UTILISEZ LE CHEMIN COMPLET VERS LA MACRO
+#[cfg_attr(feature = "user", derive(aya::Pod))] // Modifié ici
 pub enum ConnStateVariant {
     Tcp(TcpState),
     Udp(UdpState),
@@ -81,45 +77,8 @@ pub enum ConnStateVariant {
 
 #[repr(C)]
 #[derive(Clone, Copy, Debug)]
-#[cfg_attr(feature = "user", derive(aya::macros::Pod))] // UTILISEZ LE CHEMIN COMPLET VERS LA MACRO
+#[cfg_attr(feature = "user", derive(aya::Pod))] // Modifié ici
 pub struct ConnectionValue {
     pub state: ConnStateVariant,
     pub last_seen_ns: u64,
 }
-
-// Les implémentations `unsafe impl Pod for ... {}` que vous aviez pour PacketLog et IpPort
-// sont des implémentations manuelles du *trait* Pod.
-// Si vous voulez utiliser `#[derive(Pod)]`, vous devez vous assurer que la *macro* `Pod`
-// est accessible.
-
-// NOTE IMPORTANTE:
-// Si vous utilisez `#[derive(Pod)]`, vous n'avez PAS besoin de l'implémentation manuelle
-// `unsafe impl Pod for VotreType {}` pour le même type. L'un ou l'autre.
-// La macro `derive` génère cette implémentation pour vous.
-// J'ai changé vos nouvelles structures pour utiliser `derive(aya::macros::Pod)`.
-// Vous devriez faire de même pour `PacketLog` et `IpPort` si vous préférez `derive`
-// ou garder l'implémentation manuelle. Pour la cohérence, utilisons `derive` partout
-// où c'est possible.
-
-// Donc, pour PacketLog et IpPort, si vous voulez utiliser derive :
-/*
-#[repr(C)]
-#[derive(Clone, Copy)]
-#[cfg_attr(feature = "user", derive(aya::macros::Pod))] // Chemin complet
-pub struct PacketLog {
-    pub ipv4_address: u32,
-    pub action: u32,
-}
-// Plus besoin de : #[cfg(feature = "user")] unsafe impl Pod for PacketLog {}
-
-#[repr(C)]
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-#[cfg_attr(feature = "user", derive(aya::macros::Pod))] // Chemin complet
-pub struct IpPort {
-    pub addr: u32,
-    pub addr_dest: u32,
-    pub port: u16,
-    pub _pad: u16,
-}
-// Plus besoin de : #[cfg(feature = "user")] unsafe impl Pod for IpPort {}
-*/

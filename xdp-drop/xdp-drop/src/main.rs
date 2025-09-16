67dc8169f81f0c802593669a144e4368359afe31
laborifermc <laborifermc@gmail.com>
2025-09-16 10:22:21 +0200

    backup car ca fonctionne

--- xdp-drop/xdp-drop/src/main.rs
+++ xdp-drop/xdp-drop/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Context;
 use aya::{
-    Bpf,
+    Ebpf,
     include_bytes_aligned,
     maps::HashMap,
     programs::{Xdp, XdpFlags},
@@ -318,7 +318,7 @@ async fn main() -> Result<(), anyhow::Error> {
     info!("Logger initialisé.");
 
     // Chargement du programme eBPF
-    let mut bpf = Bpf::load(include_bytes_aligned!(concat!(
+    let mut bpf = Ebpf::load(include_bytes_aligned!(concat!(
         env!("OUT_DIR"),
         "/xdp-drop"
     )))

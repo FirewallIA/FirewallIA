9f254340ff15f480a5c84dcd364eec7b3f19900d
laborifermc <laborifermc@gmail.com>
2026-02-06 11:37:49 +0100

    ajout des logs envoie en gRPC

--- xdp-drop/xdp-drop-common/src/lib.rs
+++ xdp-drop/xdp-drop-common/src/lib.rs
@@ -52,5 +52,3 @@ pub const STAT_BLOCKED: u32 = 2;
 pub const STAT_TOTAL_TYPES: u32 = 3; // Taille du tableau
 
 // --- Autres définitions partagées si nécessaire ---
-#[cfg(feature = "user")]
-unsafe impl aya::Pod for PacketLog {}
